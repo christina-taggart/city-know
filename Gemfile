79411f066ae00e22667fddda22415dac7323b4c5
Aditya Mahesh & Alexander Leishman <pair+amahesh0501.leishman@devbootcamp.com>
2014-02-20 16:12:39 -0800

    created rails skeleton, with postgres

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+ruby "1.9.3"
+gem 'jquery-rails'
+
+gem 'rails', '3.2.13'
+
+gem 'pg'
+
+group :assets do
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :test, :development do
+  gem 'rspec-rails', "~> 2.0"
+end
+
+group :test do
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'selenium-webdriver'
+  gem 'factory_girl_rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CityKnow::Application.config.secret_token = '0972afa4f87d0028e574b1e5497ad23b8dda384580d8ef4c8dd9b46c1c4c4d594b426ac47fdfc84cfd0256bdc550d8530ed2a64fa7f971e16dcf5e893d6755bc'
