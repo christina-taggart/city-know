ae62e623bde3cc72b44afc0f48af7a1f49cc4ffe
Apprentice <apprentice@DBC-Infinity-2.local>
2014-02-22 18:03:09 -0800

    finished logged-in state content creation testing

--- spec/features/city_know_spec.rb
+++ spec/features/city_know_spec.rb
@@ -1,7 +1,10 @@
 require 'spec_helper'
 
 describe "City Know", js: true do
-  let!(:city) { FactoryGirl.create :city }
+  let!(:user) { FactoryGirl.create :user }
+  let!(:city) { City.create!(name: 'San Francisco', description: "The City by the Bay") }
+  let!(:question) { Question.create!(title: "Who are you?", body: "The Faunz") }
+  let!(:answer) { FactoryGirl.create :answer }
   context "Visitor can navigate the welcome screen" do
     it "by visiting the homepage" do
       visit root_path
@@ -24,20 +27,49 @@ describe "City Know", js: true do
     end
   end
 
-  # context "User can navigate the logged-in state" do
-  #   before do
+  context "User can navigate the logged-in state to create content" do
+    before(:each) do
+      ApplicationController.any_instance.stub(:current_user).and_return(user)
+      ApplicationController.any_instance.stub(:authenticate_user!).and_return(true)
+    end
+    it "and can see a list of cities" do
+      visit cities_path
+      expect(page).to  have_content :city
+    end
+    it "and can select a city" do
+      visit cities_path
+      click_link('San Francisco')
+      expect(page).to have_content "Post a new question"
+    end
+    it "and can render question template page" do
+      visit '/cities/1'
+      click_link('Post a new question')
+      expect(page).to have_content "Title"
+    end
+    it "and can post a question" do
+      visit '/cities/1/questions/new'
+      fill_in('Title', with: "What are the best things?")
+      fill_in('Body', with: "I want to know the best things.")
+      click_button('Submit')
+      expect(page).to have_content :question
+    end
+    it "and can write an answer" do
+      visit '/cities/1/questions/1'
+      fill_in("answer[body]", with: "I am the best thing")
+      click_button('Submit')
+      expect(page).to have_content :answer
+    end
+  end
+
+  # context "User can navigate the logged-in state to edit/delete content" do
+  #   before(:each) do
   #     ApplicationController.any_instance.stub(:current_user).and_return(user)
   #     ApplicationController.any_instance.stub(:authenticate_user!).and_return(true)
   #   end
-  #   let!(:user) { FactoryGirl.create :user }
-  #   it "and can see a list of cities" do
-  #     visit cities_path
-  #     expect(page).to  have_content "San Francisco"
-  #   end
-  #   it "and can select a city" do
-  #     visit cities_path
-  #     click_link('San Francisco')
-  #     expect(page).to have_content "Post a new question"
+  #   it "and can see the question edit template" do
+  #     visit '/cities/1/questions/1'
+  #     click_link('Edit Question')
+  #     expect(page).to have_content "Title"
   #   end
   # end
 end
